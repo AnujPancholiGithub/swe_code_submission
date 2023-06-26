280c9e959fdd392820c21fe866368e331051581f
AnujPancholiGithub <helpeventanuj@gmail.com>
2023-06-26 16:38:13 +0530

    fine

--- client/src/Components/Allroutes.jsx
+++ client/src/Components/Allroutes.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Login from "./Auth/Login";
 import SignUp from "./Auth/SignUp";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 
 const Allroutes = () => {
   return (
     <div>
-      <Login />
-      <SignUp />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
